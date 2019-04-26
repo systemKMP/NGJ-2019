2e47ac83c7e57c2fac05c79ec0145839e4f9483e
Asbjørn Hansen <ash@moviestarplanet.com>
2019-04-26 22:56:04 +0200

    add basic menu and level handling

---
+++ Assets/MainMenuBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Assets;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    private SceneHandler sceneHandler;
+
+    public void Awake()
+        => sceneHandler = new SceneHandler();
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit requested");
+
+        Application.Quit(0);
+    }
+
+    public void NewGame()
+    {
+        Debug.Log("New game requested");
+
+        Assert.IsNotNull(sceneHandler, "sceneHandler != null");
+
+        sceneHandler.GoToLevel(1);
+    }
+}
