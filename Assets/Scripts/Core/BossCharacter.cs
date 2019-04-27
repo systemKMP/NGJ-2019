043504882bd253e8119520d9e86e203e4543d45d
Kris <km@sportcaster.dk>
2019-04-28 01:49:17 +0200

    step sounds. enjoy.

--- Assets/Scripts/Core/BossCharacter.cs
+++ Assets/Scripts/Core/BossCharacter.cs
@@ -4,6 +4,19 @@ public class BossCharacter : PlayerCharacter
 {
     private Animator animator;
 
+    public AudioClip BossStepClip;
+    private AudioSource BossStepSource;
+
+    public AudioSource AddAudio(AudioClip clip, bool loop, bool playAwake, float vol)
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>() as AudioSource;
+        newAudio.clip = clip;
+        newAudio.loop = loop;
+        newAudio.playOnAwake = playAwake;
+        newAudio.volume = vol;
+        return newAudio;
+    }
+
     public ProjectileLauncher SecondaryLauncher;
 
     protected override void Awake()
@@ -14,6 +27,9 @@ public class BossCharacter : PlayerCharacter
 
         animator.SetBool("IsWalking", false);
         animator.SetBool("IsStunned", false);
+
+        // Add audio clips
+        BossStepSource = AddAudio(BossStepClip, false, true, 0.2f);
     }
 
     public override void Kill(bool respawn)
@@ -34,6 +50,14 @@ public class BossCharacter : PlayerCharacter
         base.TryMove(direction);
 
         animator.SetBool("IsWalking", direction != Vector2.zero);
+
+        if (direction.magnitude > 0.05f)
+        {
+            if (!BossStepSource.isPlaying)
+                BossStepSource.Play();
+        }
+        else
+            BossStepSource.Stop();
     }
 
     public override void TryStartAction(int actionIndex)
