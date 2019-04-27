9bc502767a701644c830119baf889109818325bc
Asbjørn Hansen <ash@moviestarplanet.com>
2019-04-27 14:57:22 +0200

    hook up animation and model to the villagers

--- Assets/Scripts/Core/VillagerCharacter.cs
+++ Assets/Scripts/Core/VillagerCharacter.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class VillagerCharacter : PlayerCharacter
 {
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        animator.SetBool("IsRunning", false);
+    }
+
+    protected override void Kill()
+    {
+        base.Kill();
+
+        animator.SetBool("IsDead", true);
+    }
+
+    public override void TryMove(Vector2 movement)
+    {
+        base.TryMove(movement);
+
+        animator.SetBool("IsRunning", movement != Vector2.zero);
+    }
 
     public override void TryReleaseAction(int actionIndex)
     {
